53664ce30775a1bfdf77d624334b31c58e1c8168
Jon Evans <jonathan.evans@travellocal.com>
2018-05-04 16:36:38 +0100

    Simplify tests. Add a test to make sure channels are correctly closed when publishing to a persistent connection - currently failing.

--- source/test/test.ts
+++ source/test/test.ts
@@ -2,12 +2,13 @@ import {expect} from "./chai";
 import * as sinon from "sinon";
 import {RabbitMqConnectionFactory,RabbitMqConsumer,RabbitMqProducer, IRabbitMqConnectionConfig, RabbitMqSingletonConnectionFactory} from "../index";
 import {ConsoleLogger} from "rokot-log";
-import * as Promise from "bluebird";
+import * as BluebirdPromise from "bluebird";
 import {DefaultQueueNameConfig} from "../common";
+import { connect } from "amqplib";
 
-const logger = ConsoleLogger.create("test",{level:"trace"});
-const config: IRabbitMqConnectionConfig = {host:"192.168.99.100", port:5672};
-const invalidConfig: IRabbitMqConnectionConfig = {host:"192.168.99.100", port:5670};
+const logger = ConsoleLogger.create("test", { level: "trace" });
+const config: IRabbitMqConnectionConfig = { host: "localhost", port: 5672 };
+const invalidConfig: IRabbitMqConnectionConfig = { host: "localhost", port: 5670 };
 const queueName = "TestPC";
 
 interface IMessage{
@@ -17,26 +18,29 @@ interface IMessage{
 
 describe("RabbitMqSingletonConnectionFactory Test", () => {
 
-  it("Singleton Connection Factory should return singleton connection", () => {
+  it("Singleton Connection Factory should return singleton connection", async () => {
     var f = new RabbitMqSingletonConnectionFactory(logger, config);
-    return Promise.all([f.create(),f.create(),f.create()]).then(cons => {
-      expect(cons).to.exist;
-      expect(cons.length).to.eq(3);
+    const connections = await Promise.all([f.create(),f.create(),f.create()])
+    expect(connections).to.exist;
+    expect(connections.length).to.eq(3);
 
-      cons.forEach((con, i) => {
-        expect(con).to.exist;
-        if (i > 0) {
-          expect(cons[0]).to.equal(con);
-        }
-      })
-    })
+    for (let connection of connections) {
+      expect(connection).to.exist;
+      // Since the connection is a singleton, all instances of the connection should be the same object
+      expect(connections[0]).to.equal(connection);
+    }
+
   })
 })
 
-describe("RabbitMq Test", () => {
+describe("Invalid configuration", () => {
+
+  let factory;
+  beforeEach(() => {
+    factory = new RabbitMqConnectionFactory(logger, invalidConfig);
+  });
 
   it("ConnectionFactory: Invalid Connection config should fail create", () => {
-    var factory = new RabbitMqConnectionFactory(logger, invalidConfig);
     return expect(factory.create()).to.eventually.be.rejected.then(v => {
       expect(v).to.exist;
       expect(v.code).to.eq("ECONNREFUSED");
@@ -44,8 +48,7 @@ describe("RabbitMq Test", () => {
   })
 
   it("RabbitMqConsumer: Invalid Connection config should fail subscribe", () => {
-    var factory = new RabbitMqConnectionFactory(logger, invalidConfig);
-    const consumer = new RabbitMqConsumer(logger,factory)
+    const consumer = new RabbitMqConsumer(logger, factory)
     return expect(consumer.subscribe(queueName, m => {})).to.eventually.be.rejected.then(v => {
       expect(v).to.exist;
       expect(v.code).to.eq("ECONNREFUSED");
@@ -53,7 +56,6 @@ describe("RabbitMq Test", () => {
   })
 
   it("RabbitMqProducer: Invalid Connection config should fail publish", () => {
-    var factory = new RabbitMqConnectionFactory(logger, invalidConfig);
     const producer = new RabbitMqProducer(logger,factory)
     return expect(producer.publish(queueName, {})).to.eventually.be.rejected.then(v => {
       expect(v).to.exist;
@@ -61,87 +63,96 @@ describe("RabbitMq Test", () => {
     });
   })
 
-  it("Consumer should subscribe and dispose ok with simple queue name", () => {
-    const spy = sinon.spy()
-    const factory = new RabbitMqConnectionFactory(logger, config);
-    const consumer = new RabbitMqConsumer(logger,factory)
-    return consumer.subscribe<IMessage>(queueName, spy).then(s => Promise.delay(500, s))
-      .then(disposer => {
-        expect(disposer, "disposer should exist").to.exist;
+  }
+)
 
-        expect(spy.callCount).to.be.eq(0, "Consumer spy should not have been called");
+describe("Valid configuration", () => {
 
-        return expect(disposer()).to.eventually.be.fulfilled;
-        // sub.dispose();
-        // expect(sub.isDisposed(), "Subscription should be disposed after 2nd dispose call").to.be.eq(true);
-      });
-  });
+  describe ("Consumer", () => {
 
-  it("Consumer should subscribe and dispose ok with queue config", () => {
-    const spy = sinon.spy()
-    const factory = new RabbitMqConnectionFactory(logger, config);
-    const consumer = new RabbitMqConsumer(logger,factory)
-    return consumer.subscribe<IMessage>(new DefaultQueueNameConfig(queueName), spy).then(s => Promise.delay(500, s))
-      .then(disposer => {
-        expect(disposer, "disposer should exist").to.exist;
+    let factory: RabbitMqConnectionFactory;
+    let consumer: RabbitMqConsumer;
 
-        expect(spy.callCount).to.be.eq(0, "Consumer spy should not have been called");
+    beforeEach(() => {
+      factory = new RabbitMqConnectionFactory(logger, config);
+      consumer = new RabbitMqConsumer(logger, factory);
+    });
 
-        return expect(disposer()).to.eventually.be.fulfilled;
-        // sub.dispose();
-        // expect(sub.isDisposed(), "Subscription should be disposed after 2nd dispose call").to.be.eq(true);
-      });
-  });
+    it("should subscribe and dispose ok with simple queue name", async () => {
+      const spy = sinon.spy();
+      const disposer = await consumer.subscribe<IMessage>(queueName, spy);
+      expect(disposer, "disposer should exist").to.exist;
+      expect(spy.callCount).to.be.eq(0, "Consumer spy should not have been called");
 
-  it("Consumer should recieve message from Producer", () => {
-    const spy = sinon.spy()
-    const factory = new RabbitMqConnectionFactory(logger, config);
-    const consumer = new RabbitMqConsumer(logger,factory)
-    return consumer.subscribe<IMessage>(queueName, spy).then(disposer => {
-      const producer = new RabbitMqProducer(logger,factory)
+      return expect(disposer()).to.eventually.be.fulfilled;
+    });
+
+    it("should subscribe and dispose ok with queue config", async () => {
+      const spy = sinon.spy();
+      const disposer = await consumer.subscribe<IMessage>(new DefaultQueueNameConfig(queueName), spy);
+      expect(disposer, "disposer should exist").to.exist;
+      expect(spy.callCount).to.be.eq(0, "Consumer spy should not have been called");
+
+      return expect(disposer()).to.eventually.be.fulfilled;
+    });
+
+    it("should recieve message from Producer", async () => {
+      const spy = sinon.spy()
+      const disposer = await consumer.subscribe<IMessage>(queueName, spy);
+      const producer = new RabbitMqProducer(logger, factory);
       const msg: IMessage = {data: "time", value: new Date().getTime()};
 
-      return expect(producer.publish<IMessage>(queueName, msg)).to.eventually.be.fulfilled
-      .then(() => Promise.delay(500))
-      .then(() => {
-        expect(spy.callCount).to.be.eq(1, "Consumer spy should have been called once");
-        expect(spy.firstCall.args).to.exist;
-        expect(spy.firstCall.args.length).to.be.eq(1, "Spy should have been called with message argument");
-        const consumedMsg = spy.firstCall.args[0] as IMessage;
-        expect(consumedMsg.data).to.exist;
-        expect(consumedMsg.data).to.be.eq(msg.data, "data property mismatch");
-        expect(consumedMsg.value).to.exist;
-        expect(consumedMsg.value).to.be.eq(msg.value, "value property mismatch");
-        disposer();
-      });
-    })
-  });
+      await expect(producer.publish<IMessage>(queueName, msg)).to.eventually.be.fulfilled;
+      // Welcome to integration testing - we need to wait for the message to actually be sent through RabbitMQ
+      await BluebirdPromise.delay(500);
 
-  it("Consumer should DLQ message from Producer if action fails", () => {
-    const factory = new RabbitMqConnectionFactory(logger, config);
-    const consumer = new RabbitMqConsumer(logger,factory)
-    return consumer.subscribe<IMessage>(queueName, m => Promise.reject(new Error("Test Case Error: to fail consumer subscriber message handler")))
-    .then(disposer => {
-      const producer = new RabbitMqProducer(logger,factory)
+      expect(spy.callCount).to.be.eq(1, "Consumer spy should have been called once");
+      sinon.assert.calledWithExactly(spy, msg);
+
+      disposer();
+    });
+
+    it("should DLQ message from Producer if action fails", async () => {
+      const disposer = await consumer.subscribe<IMessage>(queueName, m => BluebirdPromise.reject(new Error("A fake error that should put messages on the DLQ")));
+      const producer = new RabbitMqProducer(logger, factory);
       const msg: IMessage = {data: "time", value: new Date().getTime()};
-      return producer.publish<IMessage>(queueName, msg)
-        .then(() => Promise.delay(500))
-        .then(disposer);
-    })
+
+      await producer.publish<IMessage>(queueName, msg);
+      await BluebirdPromise.delay(500);
+
+      disposer();
+    });
+  });
+
+  describe ("Producer", () => {
+    it("should not leave channels open", async () => {
+      const factory = new RabbitMqSingletonConnectionFactory(logger, config);
+      const producer = new RabbitMqProducer(logger, factory);
+      const connection = await factory.create();
+
+      const msg: IMessage = {data: "time", value: new Date().getTime()};
+      // Publishing a message should close the channel once it's done
+      await Promise.all([
+        producer.publish<IMessage>(queueName, msg),
+        producer.publish<IMessage>(queueName, msg),
+      ]);
+
+      const channels: Array<any> = (connection as any).connection.channels;
+      expect(channels.length).to.equal(1);
+    });
+  });
+
+  after( async () => {
+    const factory = new RabbitMqConnectionFactory(logger, config);
+    const queueConfig = new DefaultQueueNameConfig(queueName);
+    const connection = await factory.create();
+    const channel = await connection.createChannel();
+    // After the tests, clear out the queue, as well as its dead letter queue, from RabbitMQ
+    await BluebirdPromise.all([
+      channel.deleteExchange(queueConfig.dlx),
+      channel.deleteQueue(queueConfig.dlq),
+      channel.deleteQueue(queueConfig.name),
+    ])
+    connection.close();
   });
 })
-
-describe("Delete Queues After tests", () => {
-  it("Delete all test queues", () => {
-    var f = new RabbitMqConnectionFactory(logger, config);
-    var d = new DefaultQueueNameConfig(queueName);
-    return f.create().then(c => {
-      return c.createChannel().then(ch=>{
-        return Promise.all([ch.deleteExchange(d.dlx), ch.deleteQueue(d.dlq), ch.deleteQueue(d.name)]).return()
-      })
-    })
-  })
-})
-
-
-//const factory = new ConnectionFactory("amqp://localhost:1234");
